30c69ffc2510f1aa4a8a5593ded126fe064097d0
Dario Caruso <darioyzf@gmail.com>
2016-12-07 16:36:04 +0100

    uninfluent changes

--- index.ts
+++ index.ts
@@ -2,8 +2,6 @@ import * as child_process from "child_process"
 
 import * as Promise from "bluebird"
 
-
-
 export default function listwifi(device: string) {
     return new Promise((resolve, reject) => {
         child_process.exec('iw dev ' + device + ' station dump', function (err, stdout, stderr) {
